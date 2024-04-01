95168698f8a0e7719350441eebfa120bc265e750
Raul Victor Trombin <raulvtrombin@gmail.com>
2024-04-01 17:13:08 -0300

    backup2

--- examples/ping_async.rs
+++ examples/ping_async.rs
@@ -5,6 +5,8 @@ use futures::stream::StreamExt;
 use std::{env, io};
 use tokio_util::codec::{Decoder, Encoder};
 
+use ping_rs::{common, decoder::Decoder as PingDecoder, message::ProtocolMessage, Messages};
+use std::convert::TryFrom;
 use tokio_serial::SerialPortBuilderExt;
 
 #[cfg(unix)]
@@ -12,23 +14,36 @@ const DEFAULT_TTY: &str = "/dev/ttyUSB0";
 #[cfg(windows)]
 const DEFAULT_TTY: &str = "COM1";
 
-struct HexCodec;
+struct PingCodec;
 
-impl Decoder for HexCodec {
-    type Item = Vec<u8>;
+impl Decoder for PingCodec {
+    type Item = Messages;
     type Error = io::Error;
 
     fn decode(&mut self, src: &mut BytesMut) -> Result<Option<Self::Item>, Self::Error> {
-        if src.len() > 0 {
-            let packet = src.split().to_vec();
-            Ok(Some(packet))
-        } else {
-            Ok(None)
+        let mut decoder = PingDecoder::new();
+
+        for byte in src.iter() {
+            match decoder.parse_byte(*byte) {
+                ping_rs::decoder::DecoderResult::InProgress => { return Ok(None) }
+                ping_rs::decoder::DecoderResult::Success((msg)) => {
+                    println!("sucess");
+                    match Messages::try_from(&msg) {
+                        Ok(Messages::Common(msg)) => return Ok(Some(Messages::Common((msg)))),
+                        Ok(Messages::Ping1D(msg)) => return Ok(Some(Messages::Ping1D((msg)))),
+                        _ => {},
+                    }
+                }
+                ping_rs::decoder::DecoderResult::Error(_) => {},
+            }
         }
+
+
+        Ok(None)
     }
 }
 
-impl Encoder<Vec<u8>> for HexCodec {
+impl Encoder<Vec<u8>> for PingCodec {
     type Error = io::Error;
 
     fn encode(&mut self, item: Vec<u8>, dst: &mut BytesMut) -> Result<(), Self::Error> {
@@ -48,11 +63,44 @@ async fn main() -> tokio_serial::Result<()> {
     port.set_exclusive(false)
         .expect("Unable to set serial port exclusive to false");
 
-    let mut reader = HexCodec.framed(port);
+    let mut reader = PingCodec.framed(port);
 
-    while let Some(packet_result) = reader.next().await {
-        let packet = packet_result.expect("Failed to read packet");
-        println!("{:02X?}", packet);
+    // // Spawn a separate task for sending messages
+    // let sender_task = task::spawn(async move {
+    //     loop {
+    //         // Send your serial message here
+    //         // For demonstration, let's send a simple message
+    //         let message = vec![0x01, 0x02, 0x03];
+    //         if let Err(err) = port.write_all(&message).await {
+    //             eprintln!("Failed to send message: {:?}", err);
+    //         }
+    //         tokio::time::sleep(Duration::from_secs(30)).await;
+    //     }
+    // });
+
+
+    while let Some(message_result) = reader.next().await {
+        let message = message_result.expect("Failed to read message");
+
+        // Handle your message here
+        match message {
+            ping_rs::Messages::Common(common::Messages::GeneralRequest(general_request)) => {
+                // Handle general request message
+                println!("General request message: {:?}", general_request);
+            }
+            ping_rs::Messages::Ping1D(something) => {
+                // Handle general request message
+                println!("General request message: {:?}", something);
+            }
+            ping_rs::Messages::Common(common::Messages::Nack(nack)) => {
+                // Handle nack message
+                println!("Nack message: {:?}", nack);
+            }
+            _ => {
+                // Handle other message types
+            }
+        }
     }
+
     Ok(())
 }
